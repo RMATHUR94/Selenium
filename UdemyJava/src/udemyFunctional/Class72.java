62d89d4087a59c9409dd545c95049569b4dd4521
vyrazu-66 <vyrazu-66@vyrazu66-ThinkCentre-M70t>
2022-12-22 13:44:53 +0530

    Class_80_change

--- UdemyJava/src/udemyFunctional/Class72.java
+++ UdemyJava/src/udemyFunctional/Class72.java
@@ -1,5 +1,6 @@
 package udemyFunctional;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -8,20 +9,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Class72 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException
+	{
 
 		System.setProperty("webdriver.chrome.driver",
 				"/home/vyrazu-66/Downloads/chromedriver_linux64 (1)/chromedriver");
 
 		WebDriver driver = new ChromeDriver();
+		WebDriverWait expwait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
 		String[] itemNeeded = { "Cucumber", "Brocolli", "Tomato" };
 
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		Thread.sleep(2000);
 
 		driver.manage().window().maximize();
 
@@ -29,8 +36,12 @@ public class Class72 {
 		
 		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
 		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();  // //*[text()='PROCEED TO CHECKOUT']
-		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("r");
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		//explicit wait
+		
+		expwait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+
 		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
 
 	}
