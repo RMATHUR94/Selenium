1574d49fe7ca519ef58947e5fffe3638e2b175d1
vyrazu-66 <vyrazu-66@vyrazu66-ThinkCentre-M70t>
2022-12-29 18:26:06 +0530

    Dec_29

---
+++ UdemyJava/src/Section10/Ass5.java
@@ -0,0 +1,22 @@
+package Section10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ass5 {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver",
+				"/home/vyrazu-66/Downloads/chromedriver_linux64 (1)/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/");
+		driver.findElement(By.linkText("Nested Frames")).click();
+		driver.switchTo().frame("frame-top");
+		driver.switchTo().frame("frame-middle");
+		System.out.println(driver.findElement(By.id("content")).getText());
+ 	}
+
+}
---
+++ UdemyJava/src/Section10/iFrameDemo92.java
@@ -0,0 +1,28 @@
+package Section10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class iFrameDemo92 {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver",
+				"/home/vyrazu-66/Downloads/chromedriver_linux64 (1)/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://jqueryui.com/droppable/");
+		System.out.println(driver.findElements(By.tagName("iframe")).size());
+		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='demo-frame']")));
+		//driver.findElement(By.id("draggable")).click();
+		Actions a = new Actions(driver);
+		WebElement source = driver.findElement(By.id("draggable"));
+		WebElement Target =driver.findElement(By.id("droppable"));
+		a.dragAndDrop(source, Target).build().perform();
+		driver.switchTo().defaultContent(); // back to old windows.
+	}
+
+}
